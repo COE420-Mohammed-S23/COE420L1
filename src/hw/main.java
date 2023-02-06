6a1d03e5ee6b36b5018947fac524b281ce078364
85143m <b00085143@aus.edu>
2023-02-06 17:44:48 +0400

    Feature B added

--- src/hw/main.java
+++ src/hw/main.java
@@ -1,10 +1,15 @@
 package hw;
 
 public class main {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 		System.out.println("Hello World");
+		System.out.println("I Member B changed this code.");
+	
+		
 	}
 
 }
